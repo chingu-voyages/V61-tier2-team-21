e018162c6ab73d99d2bd986bd5e0fe2bc876e463
wjbetech <wjbetech@gmail.com>
2026-06-12 17:28:13 +0900

    feat/game-ui: added initial game board component based on the low-fid. style documentation

---
+++ src/components/GameBoard/Board.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Tile from "./Tile";
+import type { BoardProps, BoardTile } from "../../types/board";
+
+const Row = React.memo(({ tiles }: { tiles: BoardTile[] }) => {
+  return (
+    <div className="flex flex-row gap-1">
+      {tiles.map((tile, i) => (
+        <Tile key={i} letter={tile.letter} state={tile.state} />
+      ))}
+    </div>
+  );
+});
+
+Row.displayName = "Row";
+
+const Board = React.memo(({ board }: BoardProps) => {
+  return (
+    <div className="flex flex-col gap-1">
+      {board.map((row, i) => (
+        <div className="gap-y-1">
+          <Row key={i} tiles={row} />
+        </div>
+      ))}
+    </div>
+  );
+});
+
+Board.displayName = "Board";
+
+export default Board;
