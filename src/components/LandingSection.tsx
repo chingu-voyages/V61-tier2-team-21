e29ebc8dbf94de990b27883e28ca625dbb7fee38
wjbetech <wjbetech@gmail.com>
2026-06-19 14:39:56 +0900

    feat/styles: updates as per Tinas requests, left styles alone for now

--- src/components/LandingSection.tsx
+++ src/components/LandingSection.tsx
@@ -2,13 +2,13 @@ import { Button } from "@/components/ui/button";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col items-center max-w-7xl mx-auto text-center gap-y-4">
-      <h1 className="text-5xl font-bold font-title">Questle</h1>
-      <section>
+    <section className="flex flex-col items-center max-w-7xl mx-auto text-center gap-y-4">
+      <h2 className="text-5xl font-bold font-title">Questle</h2>
+      <div>
         <Button variant="outline" className="cursor-pointer px-8 bg-accent border-gray-400 text-light-inverse">
           Play
         </Button>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 }
