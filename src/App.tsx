8b2c4ddc7291dea3b9eba1eea369beb00a981248
wjbetech <wjbetech@gmail.com>
2026-06-19 18:19:27 +0900

    feat/app-state: fixed some dud lines of code that didnt do anything

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 // core
 import { useGameStore } from "@/store/gameStore";
 import Layout from "./components/Layout/index";
-<<<<<<< HEAD
-import Welcome from "./components/LandingSection";
-=======
 import Landing from "./pages/Landing";
 import Playing from "./pages/Playing";
 import Finished from "./pages/Finished";
->>>>>>> 3dcf45d (feat/app-state: zustand game phase routing & updated codebase configuration)
 
 function App() {
   const phase = useGameStore((set) => set.phase); // eventually refactor these out
-  // const startGame = useGameStore((set) => set.startGame);
-  // const finishGame = useGameStore((set) => set.finishGame);
-  // const resetGame = useGameStore((set) => set.resetGame);
 
   return (
     <Layout>
