5228437bc84b9061d3385f1f7ce2930c2466f9eb
wjbetech <wjbetech@gmail.com>
2026-06-27 01:03:53 +0900

    chore/infra: removed /src/constants, refactored constants into data, removed dud libs/utils, renamed tailwindUtils, updated gameStore for configs

---
+++ src/data/constants.ts
@@ -0,0 +1,20 @@
+import type { GameConfig } from "@/types/game/gameConfig";
+
+export const WORD_LENGTH = {
+  MIN: 4,
+  DEFAULT: 5,
+  MAX: 6
+} as const;
+
+export const MAX_GUESSES = 6;
+
+export const DEFAULT_GAME_CONFIG: GameConfig = {
+  wordLength: WORD_LENGTH.DEFAULT,
+  maxGuesses: MAX_GUESSES
+};
+
+export const KEYBOARD_ROWS = [
+  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+  ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+  ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "BACKSPACE"]
+] as const;
