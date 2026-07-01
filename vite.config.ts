783ba222d1452272020455a25790d1b30b300a02
wjbetech <wjbetech@github.com>
2026-07-01 17:48:48 +0900

    feat/vp-migration: pushing working, migrated vp

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import path from "node:path";
-import { defineConfig } from "vitest/config";
+import { defineConfig, lazyPlugins } from "vite-plus";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 const __dirname = import.meta.dirname;
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
+  lint: {
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    options: { typeAware: true, typeCheck: true },
+  },
+  plugins: lazyPlugins(() => [react(), tailwindcss()]),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
