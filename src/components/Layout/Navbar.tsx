d368b8b5c9485405f104ac9fa2ac90fd3a38d53d
danimkim <danimkim.dev@gmail.com>
2026-07-13 00:51:15 +0100

    refactor: replace logo image with the plain text in the header

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,27 +1,22 @@
-import { Puzzle, ChartColumnBig, CogOne, InfoOctagon } from "@mynaui/icons-react";
+import { InfoOctagon, ChartBarOne } from "@mynaui/icons-react";
 
 export default function Header() {
   return (
-    <nav className="header w-full border-b bg-background">
-      <div className="mx-auto flex max-w-7xl items-center justify-between p-4">
-        <h1 className="text-2xl font-extrabold font-title">
-          <a href="#">Questle</a>
+    <header className="header w-full h-13 border-b px-8 md:px-20">
+      <nav className="flex items-center justify-between h-full">
+        <div>practice mode</div>
+        <h1>
+          <span className="text-accent font-extrabold text-3xl">Questle</span>
         </h1>
-        <div className="flex items-center gap-2 text-blue-600 md:gap-3">
+        <div className="flex items-center gap-3 md:gap-7">
           <a href="#" target="_blank" rel="noreferrer noopener">
-            <Puzzle className="size-7 text-gray-600 dark:text-white" />
+            <InfoOctagon className="size-7 text-light-primary" />
           </a>
           <a href="#" target="_blank" rel="noreferrer noopener">
-            <ChartColumnBig className="size-7 text-gray-600 dark:text-white" />
-          </a>
-          <a href="#" target="_blank" rel="noreferrer noopener">
-            <InfoOctagon className="size-7 text-amber-500" />
-          </a>
-          <a href="#" target="_blank" rel="noreferrer noopener">
-            <CogOne className="size-7 text-gray-600 dark:text-gray-200" />
+            <ChartBarOne className="size-7 text-light-primary dark:text-gray-200" />
           </a>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 }
